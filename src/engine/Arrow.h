10eceeebc5ef8dbb78422503b87d61325faf4c11
Landon <smithlandon98@gmail.com>
2020-04-03 15:41:17 -0400

    fuck you rob wallace

---
+++ src/engine/Arrow.h
@@ -0,0 +1,85 @@
+#ifndef ARROW_H
+#define ARROW_H
+
+#include <iostream>
+#include "AnimatedSprite.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Sprite.h"
+#include "Projectile.h"
+#include "Controls.h"
+#include "Player.h"
+#include "Benemy.h"
+
+using namespace std;
+
+class Arrow : public AnimatedSprite{
+
+public:
+	Arrow(Player* sayu);
+
+	virtual void update(set<SDL_Scancode> pressedKeys);
+	virtual void draw(AffineTransform &at);
+
+    void onMeleeStrike();
+    
+    // void Arrow::onEssenceStrike(Weapon* w);
+    // void Arrow::onCollision(DisplayObject* other);
+
+    void save(ofstream &out);
+
+    void setPatrolRange();
+
+    void patrol();
+
+    void moveToTarget();
+
+    bool isTargetReached();
+
+    int fire();
+
+
+	int shoot = 0;
+	/* Health and such */
+	int health = 100;
+	int maxHealth = 100;
+
+	//iFrames
+	bool iFrames = false;
+	int iFrameCount = 0;
+	int numIFrames = 0;
+
+private:
+    Player* sayu;
+    Benemy* dirt;
+    bool clean = false;
+    int state = 0;
+
+    int targX = 0;
+    int targY = 0;
+
+    int maxPatX = 0;
+    int maxPatY = 0;
+    int minPatX = 0;
+    int minPatY = 0;
+
+    double vel = 4;
+    double maxVel = 20;
+    double acc = 2;
+    double rotVel = 2;
+    double rotAcc = 2;
+    double maxRotVel = 20;
+
+    int pauseCount = 0;
+
+
+	// int oldX=0, oldY=0;
+
+	// int _jumpVel = -15;s
+
+	//void initIFrames(int numFrames);
+
+};
+
+#endif
